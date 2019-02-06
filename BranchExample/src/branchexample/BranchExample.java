e275496f535c38299f028c480aaebf27573a5122
rathomee <rathomee@sheridancollege.ca>
2019-02-06 09:35:33 -0500

    Branch

---
+++ BranchExample/src/branchexample/BranchExample.java
@@ -0,0 +1,22 @@
+/*
+ * Meet Rathod
+ *  991532630
+ *  Web Programming
+ */
+package branchexample;
+
+/**
+ *
+ * @author lenovo
+ */
+public class BranchExample {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Branch");
+    }
+    
+}
